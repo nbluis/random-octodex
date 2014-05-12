d68913a7bf7db50605eb3cf34509373679ebac90
Eduardo Bohrer <nbluisrs@gmail.com>
2014-05-12 15:08:41 -0300

    just redirecting to the octodex url instead of request it.

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 var express = require('express'),
     cat = require('octodex'),
-    http = require('http'),
-    urlParser = require('url'),
     app = express();
 
 app.get('/random', function(req, res) {
@@ -10,32 +8,8 @@ app.get('/random', function(req, res) {
       res.end();
       return;
     }
-
-    var requestOptions = urlParser.parse(url, true);
-    console.log(requestOptions);
-
-    var options = {
-      hostname: requestOptions.hostname,
-      port: 80,
-      path: requestOptions.path,
-      method: 'GET'
-    };
-
-    var imageRequest = http.request(options);
-    imageRequest.on('data', function(chunk) {
-        console.log('data');
-        res.write(chunk);
-    });
-    imageRequest.on('end', function() {
-        console.log('end');
-        res.end();
-      });
-      imageRequest.on('error', function() {
-        console.log('Error:', request, body, d);
-        res.end();
-      });
-
-    imageRequest.end();
+    res.writeHead(302, {'Location' : url});
+    res.end();
 
   });
 }).listen(8009);
